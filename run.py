3786db8646745bfe5478f58e0a3f0f5ee5fac0e8
stasandrey <staschenyuk.andrey@gmail.com>
2024-03-19 09:39:09 +0300

    Probing.

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 #! /usr/bin/python3
-print("Hello ")
+import paho.mqtt.client as paho
+import random
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+client_id = f'python-mqtt-{ random.randint(0, 1000)}'
+
+client = paho.Client(paho.CallbackAPIVersion.VERSION2)
+client.username_pw_set("homeassistant", "baziliy")
+client.on_connect = on_connect
+print(client.connect("192.168.100.77", 1883))
+
+client.publish("test", "10", qos=2)
 print("111")
