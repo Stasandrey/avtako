c939d4ef8455fc8bd9800063b392b2174bc22546
stasandrey <staschenyuk.andrey@gmail.com>
2024-03-26 15:46:00 +0300

    Новые скрипты.

---
+++ cpu.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import paho.mqtt.publish as publish
+from subprocess import check_output
+from re import findall
+
+def get_data():
+    data = check_output(["sudo","bash","/home/andrey/avtako/cpu.sh"]).decode("UTF-8")
+    return(findall("\d+",data)[0])
+
+def publish_message(topic, message):
+    print("Publishing to MQTT topic: " + topic)
+    print("Message: " + message)
+
+    publish.single(topic, message, hostname="192.168.100.83")
+
+data = get_data()
+publish_message("/orange/cpu", data)
---
+++ ram.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+awk '/MemTotal/{t=$2}/MemAvailable/{a=$2}END{print 100-100*a/t}' /proc/meminfo
